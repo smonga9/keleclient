9f0a58f765fc276bbb3dcc70a30cbdd723da8865
Stutay Monga <Stutay.Monga9@gmail.com>
2018-05-28 19:53:30 -0400

    Checkpoint-4 Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,6 +21,11 @@ class Kele
     end
 
     def get_me
-        JSON.parse((self.class.get('/users/me', headers: {"authorization" => @authtoken})).body)
+        @user_data = JSON.parse((self.class.get('/users/me', headers: {"authorization" => @authtoken})).body)
     end
+
+    def get_mentor_availability
+        @mentor_id = self.get_me["current_enrollment"]["mentor_id"]
+        @mentor_availability = JSON.parse((self.class.get("/mentors/#{@mentor_id}/student_availability", headers: {"authorization" => @authtoken})).body)
+     end
 end
