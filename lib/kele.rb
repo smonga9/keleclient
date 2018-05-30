eff6eaae3c95c458e432c34bb8c6dd810d9b2560
Stutay Monga <Stutay.Monga9@gmail.com>
2018-05-29 20:57:28 -0400

    Updated Checkpoint 4

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,7 +3,7 @@ require 'httparty'
 require 'json'
 
 class Kele
-  include HTTParty
+    include HTTParty
     base_uri 'https://www.bloc.io/api/v1'
     include JSON
 
@@ -24,8 +24,9 @@ class Kele
         @user_data = JSON.parse((self.class.get('/users/me', headers: {"authorization" => @authtoken})).body)
     end
 
-    def get_mentor_availability
-        @mentor_id = self.get_me["current_enrollment"]["mentor_id"]
-        @mentor_availability = JSON.parse((self.class.get("/mentors/#{@mentor_id}/student_availability", headers: {"authorization" => @authtoken})).body)
-     end
+    def get_mentor_availability(mentor_id)
+      response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token})
+      @mentor_availability = JSON.parse(response.body)
+      p @mentor_availability
+    end
 end
