35e89bcee47b0b1b8714667fa3cce5a57b632043
Stutay Monga <Stutay.Monga9@gmail.com>
2018-05-29 21:41:57 -0400

    Checkpoint 6 Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,11 +1,11 @@
 require 'httparty'
- require 'json'
- require './lib/roadmap.rb'
+require 'json'
+require './lib/roadmap.rb'
 
  class Kele
    include HTTParty
   include Roadmap
-  # base_uri = 'https://www.bloc.io/api/v1'
+  base_uri = 'https://www.bloc.io/api/v1'
 
    def initialize(email, password)
      options = { body: { email: email, password: password } }
@@ -24,6 +24,27 @@ require 'httparty'
      @mentor_availability = JSON.parse(response.body)
      p @mentor_availability
    end
+
+   def get_messages(arg = nil)
+    if arg == nil
+      response = self.class.get(api_url("message_threads"),
+        headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("message_threads?page=#{arg}"),
+        headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender_email, recipient_id, subject, message)
+    response = self.class.post(api_url("messages"),
+       body: {
+        sender: sender_email,
+        recipient_id: recipient_id,
+        subject: subject,
+        "stripped-text": message },
+      headers: { "authorization" => @auth_token })
+  end
 
    private
    def api_url(endpoint)
